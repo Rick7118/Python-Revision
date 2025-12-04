6cf53598fb24dd326f284b757a7cbbb55fd69b17
Rick <155312472+Rick7118@users.noreply.github.com>
2025-12-04 11:57:07 +0530

    Delete 08logicalops.py

--- 08logicalops.py
+++
@@ -1,9 +0,0 @@
-high_income = True
-good_credit = True
-
-if high_income and good_credit:
-    print("Eligible")
-else:
-    print("Not Eligible")
-    
-#bitch please i knnow how and , or and not works
